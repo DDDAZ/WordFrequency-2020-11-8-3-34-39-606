502046a6038af0f327615ebeac1e6f534a9943e2
ZFan7 <zfan7@slb.com>
2020-11-20 16:02:48 +0800

    feat: duplication: extract method

--- WordFrequencyTest/WordFrequencyGameTest.cs
+++ WordFrequencyTest/WordFrequencyGameTest.cs
@@ -5,57 +5,69 @@ namespace WordFrequencyTest
 {
     public class WordFrequencyGameTest
     {
-        [Fact]
-        public void Should_get_the_1_when_input_the()
-        {
-            //Given
-            string inputStr = "the";
-            string expectResult = "the 1";
-            Validate_Input_words_process_to_expected_word(inputStr, expectResult);
-        }
+        //[Fact]
+        //public void Should_get_the_1_when_input_the()
+        //{
+        //    //Given
+        //    string inputStr = "the";
+        //    string expectResult = "the 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
 
-        [Fact]
-        public void Should_process_two_words()
-        {
-            //Given
-            string inputStr = "the is";
-            string expectResult = "the 1\nis 1";
-            Validate_Input_words_process_to_expected_word(inputStr, expectResult);
-        }
+        //[Fact]
+        //public void Should_process_two_words()
+        //{
+        //    //Given
+        //    string inputStr = "the is";
+        //    string expectResult = "the 1\nis 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
 
-        [Fact]
-        public void Should_process_two_words_with_special_spaces()
-        {
-            //Given
-            string inputStr = "the      is";
-            string expectResult = "the 1\nis 1";
-            Validate_Input_words_process_to_expected_word(inputStr, expectResult);
-        }
+        //[Fact]
+        //public void Should_process_two_words_with_special_spaces()
+        //{
+        //    //Given
+        //    string inputStr = "the      is";
+        //    string expectResult = "the 1\nis 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
 
-        [Fact]
-        public void Should_process_two_words_with_special_enter()
-        {
-            //Given
-            string inputStr = "the   \n   is";
-            string expectResult = "the 1\nis 1";
-            Validate_Input_words_process_to_expected_word(inputStr, expectResult);
-        }
+        //[Fact]
+        //public void Should_process_two_words_with_special_enter()
+        //{
+        //    //Given
+        //    string inputStr = "the   \n   is";
+        //    string expectResult = "the 1\nis 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
 
-        [Fact]
-        public void Should_pracess_two_same_words_with_sorted()
-        {
-            //Given
-            string inputStr = "the the is";
-            string expectResult = "the 2\nis 1";
-            Validate_Input_words_process_to_expected_word(inputStr, expectResult);
-        }
+        //[Fact]
+        //public void Should_pracess_two_same_words_with_sorted()
+        //{
+        //    //Given
+        //    string inputStr = "the the is";
+        //    string expectResult = "the 2\nis 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
 
-        [Fact]
-        public void Should_process_sorted_with_count_descending()
+        //[Fact]
+        //public void Should_process_sorted_with_count_descending()
+        //{
+        //    //Given
+        //    string inputStr = "the is is";
+        //    string expectResult = "is 2\nthe 1";
+        //    Validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        //}
+
+        [Theory]
+        [InlineData("the is is", "is 2\nthe 1")]
+        [InlineData("the the is", "the 2\nis 1")]
+        [InlineData("the   \n   is", "the 1\nis 1")]
+        [InlineData("the      is", "the 1\nis 1")]
+        [InlineData("the is", "the 1\nis 1")]
+        [InlineData("the", "the 1")]
+        public void Should_pass_test(string inputStr, string expectResult)
         {
-            //Given
-            string inputStr = "the is is";
-            string expectResult = "is 2\nthe 1";
             Validate_Input_words_process_to_expected_word(inputStr, expectResult);
         }
 
